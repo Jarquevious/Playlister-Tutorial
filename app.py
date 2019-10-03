1f1cc70f0460131b1306c835eb83df11bdb90473
Jarquevious <jarquevious@gmail.com>
2019-10-02 18:57:26 -0700

    Added MongoDB and Playlist model

--- app.py
+++ app.py
@@ -2,19 +2,33 @@ from pymongo import MongoClient
 client = MongoClient()
 db = client.Playlister
 playlists = db.playlists
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-# playlists = [
-#     { 'title': 'Cat Videos', 'description': 'Cats acting weird'},
-#     { 'title': '80\'s Music', 'description': 'Don\'t stop Believing!'}
-# ]
+playlists = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird'},
+    { 'title': '80\'s Music', 'description': 'Don\'t stop Believing!'}
+]
 
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
-    return render_template('playlists_index.html', playlists=playlists)
+    return render_template('playlists_index.html', playlists = playlists.find())
 
+@app.route('/playlists/new')
+def playlists_new():
+    return render_template('playlists_new.html')
+
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    """Submit a new playlist."""
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlists.insert_one(playlist)
+    return redirect(url_for('playlists_index'))
 
 if __name__== '__main__':
     app.run(debug=True)
